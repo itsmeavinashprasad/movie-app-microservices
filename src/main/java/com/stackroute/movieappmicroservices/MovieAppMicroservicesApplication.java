effde48134fb8a76066300493970010beef42523
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-10-10 16:46:02 +0530

    added modules

---
+++ src/main/java/com/stackroute/movieappmicroservices/MovieAppMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.movieappmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppMicroservicesApplication.class, args);
+	}
+
+}
