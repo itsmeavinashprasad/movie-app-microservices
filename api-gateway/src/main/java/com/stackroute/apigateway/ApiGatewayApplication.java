f04d80bbfc3f7bcff94192e82b773a2e2e7813c7
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-10-11 12:52:37 +0530

    random

--- api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/stackroute/apigateway/ApiGatewayApplication.java
@@ -6,11 +6,13 @@ import com.stackroute.apigateway.filters.PreFilter;
 import com.stackroute.apigateway.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
