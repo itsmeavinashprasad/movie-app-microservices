6995f5f14994d96ecc725127432d5afd1de33731
saptarshi.majumdar <sapgit97@gmail.com>
2019-10-11 11:20:13 +0530

    Added config server

---
+++ movie-Services/src/main/java/com/stackroute/movieservices/seeddata/PreFillApplicationListener.java
@@ -0,0 +1,46 @@
+//package com.stackroute.movieservices.seeddata;
+//
+//import com.stackroute.movieservices.domain.Movie;
+//import com.stackroute.movieservices.repository.MovieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class PreFillApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+//
+//    @Autowired
+//    private MovieRepository movieRepository;
+//
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+//        System.out.println("-> Pre-Fill using ApplicationListener<ContextRefreshedEvent>");
+//        movieRepository.save(new Movie(
+//                1,
+//                "Movie 1",
+//                (float) 7.1,
+//                "2009-01-01",
+//                true,
+//                 "Overview 1"
+//        ));
+//
+//        movieRepository.save(new Movie(
+//                2,
+//                "Movie 2",
+//                (float) 7.2,
+//                "2009-02-02",
+//                true,
+//                "Overview 2"
+//        ));
+//        movieRepository.save(new Movie(
+//                3,
+//                "Movie 3",
+//                (float) 7.3,
+//                "2009-03-03",
+//                false,
+//                "Overview 3"
+//        ));
+//
+//    }
+//}
---
+++ movie-Services/src/main/java/com/stackroute/movieservices/seeddata/PreFillCommandLineRunner.java
@@ -0,0 +1,72 @@
+//package com.stackroute.movieservices.seeddata;
+//
+//import com.stackroute.movieservices.domain.Movie;
+//import com.stackroute.movieservices.repository.MovieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@PropertySource(value = "classpath:application.properties")
+//public class PreFillCommandLineRunner implements CommandLineRunner {
+//
+//    @Autowired
+//    private MovieRepository movieRepository;
+//
+//
+//    @Value("${title}")
+//    private String title;
+//
+//    @Value("${voteAverage}")
+//    private float voteAverage;
+//
+//    @Value("${releaseDate}")
+//    private String releaseDate;
+//
+//    @Value("${adult}")
+//    private boolean adult;
+//
+//    @Value("${overview}")
+//    private String overview;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("-> Pre-Fill using CommandLineRunner");
+//        movieRepository.save(new Movie(
+//                4,
+//                "Movie 4",
+//                (float) 7.4,
+//                "2009-04-04",
+//                true,
+//                "Overview 4"
+//        ));
+//
+//        movieRepository.save(new Movie(
+//                5,
+//                "Movie 5",
+//                (float) 7.5,
+//                "2009-05-05",
+//                true,
+//                "Overview 5"
+//        ));
+//        movieRepository.save(new Movie(
+//                6,
+//                "Movie 6",
+//                (float) 7.6,
+//                "2009-06-06",
+//                false,
+//                "Overview 6"
+//        ));
+//        movieRepository.save(new Movie(
+//                0,
+//                title,
+//                voteAverage,
+//                releaseDate,
+//                adult,
+//                overview
+//        ));
+//    }
+//}
